0c0063515a3a1bb640600b4f682df1d1e474f164
apolhill <andy@thatguynamedandy.com>
2014-02-13 06:52:40 +0000

    A few tweaks to smooth animation

--- app/scripts/filters/vertical.js
+++ app/scripts/filters/vertical.js
@@ -1,25 +1,30 @@
 'use strict';
 
+/* Vertical Filter: Provides a simple up/down filter
+ */
+
 angular.module('casino.filters').
   filter('vertical', function() {
-    return function(lights, allLights, frame, maxFrame) {
+    return function(lights, allLights, frame) {
 
       var filtered = [],
-          verticalStep = 8; //approx 8% between steps
+          maxFrame = 100, //FIXME: frame animation
+          verticalStep = 2, //FIXME: approx 4% between steps
+          frame = frame % maxFrame,
+          isUp = Math.round(frame / maxFrame) ? true : false,
+          upFrame = (maxFrame - frame) * verticalStep,
+          downFrame = frame * verticalStep;
 
       angular.forEach(allLights, function(light) {
 
-        light.power = 'on';
+        light.power = 'off';
 
-        if((maxFrame)/2 > frame) {  //lights up
-          if(parseInt(light.pos.bottom) < (frame * verticalStep)) {
-            light.power = 'off';
-          }
+        if(isUp && parseInt(light.pos.bottom, 10) < upFrame) {
+          light.power = 'on';
+        }
 
-        } else {  //lights down
-          if(parseInt(light.pos.bottom) < ((maxFrame - frame) * verticalStep)) {
-            light.power = 'off';
-          }
+        if(!isUp && parseInt(light.pos.bottom, 10) < downFrame) {
+          light.power = 'on';
         }
 
         filtered.push(light);
