0249b11fcea2bcc0ecb52f9a75232e0ab35e95c8
Andy Polhill <apolhill@gmail.com>
2015-02-18 08:14:19 +0000

    Uppercase characters mapped

--- fonts/casino-raleway.js
+++ fonts/casino-raleway.js
@@ -15,10 +15,29 @@ angular.module('casino-lights')
         {'bottom':71, 'left':62},
         {'bottom':90, 'left':50}
       ],
-      'B':[],
+      'B':[
+        {'bottom':9, 'left':20},
+        {'bottom':29, 'left':20},
+        {'bottom':52, 'left':20},
+        {'bottom':71, 'left':20},
+        {'bottom':92, 'left':20},
+
+        {'bottom':9, 'left':40},
+        {'bottom':9, 'left':60},
+        {'bottom':15, 'left':80},
+        {'bottom':35, 'left':83},
+
+        {'bottom':92, 'left':40},
+        {'bottom':92, 'left':60},
+        {'bottom':85, 'left':80},
+        {'bottom':65, 'left':78},
+
+        {'bottom':52, 'left':40},
+        {'bottom':52, 'left':60}
+      ],
       'C':[
         {'bottom':50, 'left':16},
-        {'bottom':8, 'left':55},
+        {'bottom':9, 'left':55},
         {'bottom':93, 'left':55},
 
         {'bottom':30, 'left':20},
@@ -29,24 +48,152 @@ angular.module('casino-lights')
         {'bottom':70, 'left':20},
         {'bottom':86, 'left':33}
       ],
-      'D':[],
-      'E':[],
-      'F':[],
-      'G':[],
-      'H':[],
+      'D':[
+        {'bottom':9, 'left':20},
+        {'bottom':29, 'left':20},
+        {'bottom':52, 'left':20},
+        {'bottom':71, 'left':20},
+        {'bottom':92, 'left':20},
+
+        {'bottom':9, 'left':40},
+        {'bottom':10, 'left':60},
+        {'bottom':22, 'left':76},
+
+        {'bottom':92, 'left':40},
+        {'bottom':90, 'left':60},
+        {'bottom':78, 'left':76},
+
+        {'bottom':40, 'left':85},
+        {'bottom':60, 'left':85}
+      ],
+      'E':[
+        {'bottom':9, 'left':23},
+        {'bottom':29, 'left':23},
+        {'bottom':52, 'left':23},
+        {'bottom':71, 'left':23},
+        {'bottom':92, 'left':23},
+
+        {'bottom':9, 'left':43},
+        {'bottom':9, 'left':63},
+        {'bottom':9, 'left':82},
+
+        {'bottom':92, 'left':43},
+        {'bottom':92, 'left':63},
+        {'bottom':92, 'left':82},
+
+        {'bottom':52, 'left':47},
+        {'bottom':52, 'left':69}
+      ],
+      'F':[
+        {'bottom':9, 'left':24},
+        {'bottom':29, 'left':24},
+        {'bottom':51, 'left':24},
+        {'bottom':71, 'left':24},
+        {'bottom':92, 'left':24},
+
+        {'bottom':92, 'left':43},
+        {'bottom':92, 'left':63},
+        {'bottom':92, 'left':82},
+
+        {'bottom':51, 'left':47},
+        {'bottom':51, 'left':69}
+      ],
+      'G':[
+        {'bottom':50, 'left':15},
+        {'bottom':9, 'left':54},
+        {'bottom':93, 'left':54},
+
+        {'bottom':30, 'left':19},
+        {'bottom':16, 'left':32},
+        {'bottom':84, 'left':77},
+
+        {'bottom':72, 'left':16},
+        {'bottom':86, 'left':32},
+        {'bottom':16, 'left':72},
+        {'bottom':9, 'left':86},
+        {'bottom':28, 'left':86},
+        {'bottom':46, 'left':86},
+        {'bottom':46, 'left':68},
+      ],
+      'H':[
+        {'bottom':9, 'left':19},
+        {'bottom':30, 'left':19},
+        {'bottom':52, 'left':19},
+        {'bottom':72, 'left':19},
+        {'bottom':92, 'left':19},
+        {'bottom':9, 'left':81},
+
+        {'bottom':30, 'left':81},
+        {'bottom':52, 'left':81},
+        {'bottom':72, 'left':81},
+        {'bottom':92, 'left':81},
+
+        {'bottom':52, 'left':39},
+        {'bottom':52, 'left':59},
+      ],
       'I':[
-        {'bottom':10, 'left':50},
+        {'bottom':9, 'left':50},
         {'bottom':30, 'left':50},
         {'bottom':50, 'left':50},
         {'bottom':70, 'left':50},
         {'bottom':90, 'left':50}
       ],
-      'J':[],
-      'K':[],
-      'L':[],
-      'M':[],
+      'J':[
+        {'bottom':12, 'left':56},
+        {'bottom':9, 'left':24},
+        {'bottom':30, 'left':72},
+        {'bottom':50, 'left':72},
+        {'bottom':70, 'left':72},
+        {'bottom':90, 'left':72}
+      ],
+      'K':[
+        {'bottom':9, 'left':20},
+        {'bottom':30, 'left':20},
+        {'bottom':50, 'left':20},
+        {'bottom':70, 'left':20},
+        {'bottom':90, 'left':20},
+        {'bottom':55, 'left':47},
+        {'bottom':73, 'left':63},
+        {'bottom':90, 'left':78},
+        {'bottom':39, 'left':62},
+        {'bottom':23, 'left':74},
+        {'bottom':9, 'left':85}
+      ],
+      'L':[
+        {'bottom':9, 'left':24},
+        {'bottom':29, 'left':24},
+        {'bottom':52, 'left':24},
+        {'bottom':71, 'left':24},
+        {'bottom':92, 'left':24},
+
+        {'bottom':9, 'left':43},
+        {'bottom':9, 'left':63},
+        {'bottom':9, 'left':82},
+
+      ],
+      'M':[
+        {'bottom':9, 'left':17},
+        {'bottom':29, 'left':17},
+        {'bottom':52, 'left':17},
+        {'bottom':71, 'left':17},
+        {'bottom':92, 'left':17},
+
+        {'bottom':71, 'left':30},
+        {'bottom':51, 'left':39},
+
+        {'bottom':71, 'left':69},
+        {'bottom':51, 'left':61},
+        {'bottom':31, 'left':50},
+
+        {'bottom':9, 'left':83},
+        {'bottom':29, 'left':83},
+        {'bottom':52, 'left':83},
+        {'bottom':71, 'left':83},
+        {'bottom':92, 'left':83}
+
+      ],
       'N':[
-        {'bottom':10, 'left':18},
+        {'bottom':9, 'left':18},
         {'bottom':30, 'left':18},
         {'bottom':50, 'left':18},
         {'bottom':70, 'left':18},
@@ -54,14 +201,14 @@ angular.module('casino-lights')
         {'bottom':30, 'left':65},
         {'bottom':50, 'left':50},
         {'bottom':70, 'left':36},
-        {'bottom':10, 'left':82},
+        {'bottom':9, 'left':82},
         {'bottom':30, 'left':82},
         {'bottom':50, 'left':82},
         {'bottom':70, 'left':82},
         {'bottom':90, 'left':82}
       ],
       'O':[
-        {'bottom':8, 'left':50},
+        {'bottom':9, 'left':50},
         {'bottom':14, 'left':31},
         {'bottom':30, 'left':18},
         {'bottom':50, 'left':14},
@@ -74,9 +221,52 @@ angular.module('casino-lights')
         {'bottom':70, 'left':82},
         {'bottom':86, 'left':70}
       ],
-      'P':[],
-      'Q':[],
-      'R':[],
+      'P':[
+        {'bottom':9, 'left':22},
+        {'bottom':29, 'left':22},
+        {'bottom':52, 'left':22},
+        {'bottom':71, 'left':22},
+        {'bottom':92, 'left':22},
+        {'bottom':92, 'left':42},
+        {'bottom':92, 'left':62},
+        {'bottom':85, 'left':79},
+        {'bottom':68, 'left':84},
+        {'bottom':51, 'left':79},
+        {'bottom':43, 'left':62},
+        {'bottom':43, 'left':42}
+      ],
+      'Q':[
+        {'bottom':9, 'left':50},
+        {'bottom':14, 'left':31},
+        {'bottom':30, 'left':18},
+        {'bottom':50, 'left':14},
+        {'bottom':70, 'left':18},
+        {'bottom':86, 'left':30},
+        {'bottom':92, 'left':50},
+        {'bottom':14, 'left':69},
+        {'bottom':30, 'left':82},
+        {'bottom':50, 'left':86},
+        {'bottom':70, 'left':82},
+        {'bottom':86, 'left':70},
+        {'bottom':9, 'left':82},
+        {'bottom':30, 'left':63}
+      ],
+      'R':[
+        {'bottom':9, 'left':22},
+        {'bottom':29, 'left':22},
+        {'bottom':52, 'left':22},
+        {'bottom':71, 'left':22},
+        {'bottom':92, 'left':22},
+        {'bottom':92, 'left':41},
+        {'bottom':92, 'left':60},
+        {'bottom':85, 'left':76},
+        {'bottom':68, 'left':81},
+        {'bottom':51, 'left':76},
+        {'bottom':43, 'left':60},
+        {'bottom':43, 'left':41},
+        {'bottom':25, 'left':69},
+        {'bottom':9, 'left':80}
+      ],
       'S':[
         {'bottom':13, 'left':23},
         {'bottom':7, 'left':50},
@@ -88,12 +278,96 @@ angular.module('casino-lights')
         {'bottom':16, 'left':77},
         {'bottom':40, 'left':78}
       ],
-      'T':[],
-      'U':[],
-      'V':[],
-      'W':[],
-      'X':[],
-      'Y':[],
-      'Z':[]
+      'T':[
+        {'bottom':9, 'left':50},
+        {'bottom':30, 'left':50},
+        {'bottom':50, 'left':50},
+        {'bottom':70, 'left':50},
+        {'bottom':92, 'left':50},
+        {'bottom':92, 'left':31},
+        {'bottom':92, 'left':12},
+        {'bottom':92, 'left':69},
+        {'bottom':92, 'left':88}
+      ],
+      'U':[
+        {'bottom':14, 'left':30},
+        {'bottom':31, 'left':20},
+        {'bottom':52, 'left':18},
+        {'bottom':71, 'left':18},
+        {'bottom':92, 'left':18},
+        {'bottom':14, 'left':70},
+        {'bottom':31, 'left':81},
+        {'bottom':52, 'left':83},
+        {'bottom':71, 'left':83},
+        {'bottom':92, 'left':83},
+        {'bottom':9, 'left':50},
+
+      ],
+      'V':[
+        {'bottom':92, 'left':15},
+        {'bottom':72, 'left':23},
+        {'bottom':92, 'left':86},
+        {'bottom':72, 'left':78},
+        {'bottom':52, 'left':31},
+        {'bottom':52, 'left':69},
+        {'bottom':32, 'left':38},
+        {'bottom':32, 'left':62},
+        {'bottom':9, 'left':50}
+      ],
+      'W':[
+        {'bottom':92, 'left':9},
+        {'bottom':72, 'left':15},
+        {'bottom':92, 'left':61},
+        {'bottom':72, 'left':57},
+        {'bottom':52, 'left':20},
+        {'bottom':52, 'left':50},
+        {'bottom':32, 'left':24},
+        {'bottom':32, 'left':41},
+        {'bottom':9, 'left':33},
+        {'bottom':92, 'left':39},
+        {'bottom':72, 'left':44},
+        {'bottom':92, 'left':91},
+        {'bottom':72, 'left':85},
+        {'bottom':52, 'left':80},
+        {'bottom':32, 'left':59},
+        {'bottom':32, 'left':75},
+        {'bottom':9, 'left':67}
+      ],
+      'X':[
+        {'bottom':92, 'left':17},
+        {'bottom':72, 'left':32},
+        {'bottom':92, 'left':84},
+        {'bottom':72, 'left':68},
+        {'bottom':9, 'left':17},
+        {'bottom':29, 'left':32},
+        {'bottom':29, 'left':68},
+        {'bottom':9, 'left':84},
+        {'bottom':50, 'left':50},
+      ],
+      'Y':[
+        {'bottom':9, 'left':50},
+        {'bottom':29, 'left':50},
+        {'bottom':49, 'left':40},
+        {'bottom':49, 'left':60},
+        {'bottom':72, 'left':28},
+        {'bottom':72, 'left':72},
+        {'bottom':92, 'left':18},
+        {'bottom':92, 'left':84}
+      ],
+      'Z':[
+        {'bottom':92, 'left':16},
+        {'bottom':92, 'left':37},
+        {'bottom':92, 'left':58},
+        {'bottom':92, 'left':79},
+
+        {'bottom':31, 'left':32},
+        {'bottom':50, 'left':49},
+        {'bottom':69, 'left':68},
+
+        {'bottom':9, 'left':16},
+        {'bottom':9, 'left':37},
+        {'bottom':9, 'left':58},
+        {'bottom':9, 'left':79}
+      ]
     };
   });
